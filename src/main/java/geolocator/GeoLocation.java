0912df473b924e8a16428e3680f6fcac7d258e1f
RakeMeUp <bencehetes@gmail.com>
2023-02-17 12:37:37 +0100

    init

---
+++ src/main/java/geolocator/GeoLocation.java
@@ -0,0 +1,121 @@
+package geolocator;
+
+public class GeoLocation {
+
+    private String ip;
+    private String countryCode;
+    private String countryName;
+    private String regionCode;
+    private String regionName;
+    private String city;
+    private String zipCode;
+    private String timeZone;
+    private double latitude;
+    private double longitude;
+    private Integer metroCode;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getRegionCode() {
+        return regionCode;
+    }
+
+    public void setRegionCode(String regionCode) {
+        this.regionCode = regionCode;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getMetroCode() {
+        return metroCode;
+    }
+
+    public void setMetroCode(Integer metroCode) {
+        this.metroCode = metroCode;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "GeoLocation{" +
+                "ip='" + ip + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", countryName='" + countryName + '\'' +
+                ", regionCode='" + regionCode + '\'' +
+                ", regionName='" + regionName + '\'' +
+                ", city='" + city + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", timeZone='" + timeZone + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", metroCode=" + metroCode +
+                '}';
+    }
+}
